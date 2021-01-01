9a6d0a4608d0e6c4ddded0dc9daeba63c32ea45c
GeorgianBadita <geo.badita@gmail.com>
2021-01-01 13:18:21 +0000

    Remove algorithmic problems - create different repository for those

--- Algorithmic-Problems/RT1/8.cpp
+++
@@ -1,4 +0,0 @@
-/*
-pot spune sincer ca si acum ma chiorasc in carte si nu-mi dau seama ce zice =)) sa-l
-ia naiba pe Laguerre
-/*
--- Algorithmic-Problems/ore/ore.cpp
+++
@@ -1,7 +0,0 @@
-#include<cstdio>
-using namespace std;
-int main()
-{
-    freopen("ore.in","r",stdin);
-    freopen("ore.out","w",stdout);
-}
